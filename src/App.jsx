437d014b47aebea8be3735db37cb14e4e8d670d0
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-04-02 12:50:54 +0500

    feat: add React Router with Home and RecipeDetail routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import RecipeDetail from './pages/RecipeDetail'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <Navbar />   
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
     </>
   )
 }
