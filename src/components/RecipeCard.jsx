b866ef02fc1037cb14f429914ca456dc494de7a7
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-04-02 20:13:12 +0500

    feat: build RecipeDetail page with dynamic routing

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const RecipeCard = ({ recipe }) => {
+  const navigate = useNavigate()
+
+
   return (
-    <div>
+    <div onClick={() => navigate(`/recipe/${recipe.id}`)} style={{ cursor: 'pointer' }}> 
       <h2>{recipe.title}</h2>
       <img src={recipe.image} alt={recipe.title} />
     </div>
--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const RecipeDetail = () => {
+    const { id } = useParams() //access dynamic parameters from the current URL
+    const [recipe, setRecipe] = React.useState(null); // returns a single recipe object, not an array, so we initialize it as null instead of an empty array
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(null);
+
+    React.useEffect(() => {
+      setLoading(true);
+        fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=ef8cd867a22541cf8424f6b4c4cd361b`)
+        .then(res => res.json())
+        .then(data => {
+            setRecipe(data)
+            setLoading(false);
+        })
+        .catch(err => {
+            setError('Something went wrong. Please try again.');
+            setLoading(false);
+        })
+    }, [id])
+
   return (
-    <div>RecipeDetail</div>
+    <>
+    {loading ? <p>Loading...</p> :  
+        recipe && (
+            <div>
+                <h1>{recipe.title}</h1>
+                <img src={recipe.image} alt={recipe.title} />
+                <p>{recipe.instructions}</p>
+            </div>
+        )}
+        {error && <p>{error}</p>} {/*  If error is not null, show it. */}
+    </>
+
+
+
   )
 }
 
