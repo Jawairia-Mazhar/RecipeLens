46860752283afc68f87fdf3e702e48cd39617801
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-04-10 12:24:34 +0500

    feat: prevent navigation on favorite button click in RecipeCard component

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -9,15 +9,16 @@ const RecipeCard = ({ recipe, favorites, addToFav, removeFromFav }) => {
   return (
     <div onClick={() => navigate(`/recipe/${recipe.id}`)} className='cursor-pointer overflow-hidden flex flex-col gap-2 items-center border border-gray-300 rounded-md p-2 w-78'> 
       <img src={recipe.image} alt={recipe.title} />
-
+      
       <div className='flex justify-between items-center w-full'>
         <h2>{recipe.title}</h2>
         <button 
-        onClick={() => isFavorite ? removeFromFav(recipe.id) : addToFav(recipe)} 
-        className = "w-5 h-5">
+          onClick={(e) => {            
+            e.stopPropagation(); // This prevents the click event from bubbling up to the parent div, which would trigger the navigation to the recipe detail page. By calling e.stopPropagation(), we ensure that when the user clicks on the favorite button, it only toggles the favorite status without navigating away from the current page.
+            isFavorite ? removeFromFav(recipe.id) : addToFav(recipe)}}
+            className = "w-5 h-5">
           <img src={isFavorite ? FilledFav : EmptyFav} alt={isFavorite ? "Remove from Favorites" : "Add to Favorites"} />
         </button>
-
       </div>
     </div>
   )
