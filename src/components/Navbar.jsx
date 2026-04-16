cffeffaca2638586f1c5a0eca6023175ef819c46
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-04-16 23:32:22 +0500

    feat: enhance Navbar component with dynamic background and title styling based on current route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import FavoriteIcon from '../assets/favorite.png'
+import { Link, useLocation } from 'react-router-dom'
+import FavoriteIcon from '../assets/filled_heart.png'
 
 const Navbar = () => {
+  const location = useLocation() // Get the current location object
+  const isHome = location.pathname === '/' // Check if the current path is the home page
+
   return (
-    <nav className="fixed top-0 left-0 right-0 z-20 flex items-center justify-between w-full px-4 py-3 bg-transparent shadow-none">
-      <h1><Link to="/" className='text-3xl font-semibold text-white'>RecipeLens</Link></h1>
+    <nav className={`fixed top-0 left-0 right-0 z-20 flex items-center justify-between w-full px-4 py-3 
+      ${isHome ? 'bg-transparent shadow-none' : 
+      'bg-white/90 backdrop-blur-md border-b border-slate-200 shadow-sm'
+    }`}>
+      <h1><Link to="/" className={`text-3xl
+          ${isHome ? 'text-white' : 'text-gray-800'}`} style={{ fontFamily: "Pacifico", fontSize: "32px"}}>RecipeLens</Link></h1>
       
       <Link to="/favorites">
-        <img src={FavoriteIcon} alt="Favorites" className='w-6 h-6 brightness-0 invert'/>
+        <img src={FavoriteIcon} alt="Favorites" className='w-6 h-6 '/>
       </Link>
     </nav>
   )
