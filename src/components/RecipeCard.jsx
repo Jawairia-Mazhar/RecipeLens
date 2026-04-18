49bb6d2649080466fdfa6341c460400b6f55b671
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-04-18 16:33:01 +0500

    feat: enhance styling and layout of Navbar, RecipeCard, and RecipeDetail. Also add comments for better understanding

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -8,20 +8,20 @@ const RecipeCard = ({ recipe, favorites, addToFav, removeFromFav }) => {
   const isFavorite = recipe ? favorites.some((fav) => fav.id === recipe.id) : false;
 
   return (
-    <div className='card cursor-pointer overflow-hidden flex flex-col gap-2 items-center border border-gray-300 rounded-md p-2 w-70'>
+    <div className='card cursor-pointer overflow-hidden flex flex-col gap-2 items-center border border-gray-300 rounded-md w-70'>
       <div className='card__shine' />
       <div className='card__glow' />
-      <div
-        onClick={() => navigate(`/recipe/${recipe.id}`)}
-        className='card__content p-1'>
 
-        <div className='relative group'>
+      <div onClick={() => navigate(`/recipe/${recipe.id}`)}
+        className='card__content p-1'>
+{/* image div */}
+        <div className='relative group item-center'>
           <div className='card__image w-58 h-58 rounded-full overflow-hidden'>
             <img src={recipe.image} alt={recipe.title}
                 className="w-full h-full rounded-full object-cover object-center border-4 border-white shadow-lg"/>
           </div>
-        
-          <div className='flex justify-center items-center w-56 h-56 rounded-full absolute inset-0 bg-transparent backdrop-blur-xs bg-opacity-50 opacity-0 group-hover:opacity-90 transition-opacity duration-200'>
+{/* shadow div for hover effect on recipe image        */}
+          <div className='flex justify-center items-center w-58 h-58 rounded-full absolute inset-0 bg-transparent backdrop-blur-xs bg-opacity-50 opacity-0 group-hover:opacity-90 transition-opacity duration-200'>
             <button 
               onClick={(e) => {            
                 e.stopPropagation();
@@ -31,8 +31,9 @@ const RecipeCard = ({ recipe, favorites, addToFav, removeFromFav }) => {
             </button>
           </div>
         </div>
-        <div className='card__text w-full'>
-          <h2 className='card__title text-center line-clamp-2'>{recipe.title}</h2>
+{/* recipe title */}
+        <div className='card__text w-full pb-4 px-1'>
+          <h2 className='card__title text-center line-clamp-2 w-full'>{recipe.title}</h2>
         </div>
       </div>
     </div>
